f5bf41a3a7233169fcece747c197fd6367e9b870
kemusWu <42734477+kemusWu@users.noreply.github.com>
2021-05-31 17:39:15 +0800

    初版

---
+++ js/site.js
@@ -0,0 +1,72 @@
+﻿// init plugin
+new WOW().init();
+var s = skrollr.init();
+$('.js-tilt').tilt();
+
+$(function () {
+    var toTopButton = $('#to-top');
+
+    $(window).on('scroll', function () {
+        // toTop fadeIn
+        if ($(this).scrollTop() > 120) {
+            toTopButton.fadeIn();
+        } else {
+            toTopButton.fadeOut();
+        }
+    });
+
+    // toTop click
+    toTopButton.on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+
+    // hamburger click
+    $('#hamburger').click(function () {
+        $(this).toggleClass('click');
+        $('#nav').toggleClass('show');
+    });
+
+    if ($(window).width() <= 768) {
+        s.destroy();
+    }
+
+    // page nav active
+    var pageNav = $('#page-nav a');
+    if(pageNav.length!==0) {
+        var currentPath = location.pathname;
+
+        pageNav.each(function () {
+            if ('/' + $(this).attr('href') == currentPath) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    }
+    
+    // 执行I18n翻译
+    execI18n();
+
+
+    $('#language span').each(function () {
+        if ($(this).attr('lang') == i18nLanguage) {
+            $('#page .page-nav-container').addClass(i18nLanguage);
+            $('#nav .nav-container').addClass(i18nLanguage);
+            $('body').addClass(i18nLanguage);
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+
+        $(this).on('click', function () {
+            var language = $(this).attr('lang');
+            getCookie("userLanguage", language, {
+                expires: 30,
+                path: '/'
+            });
+            location.reload();
+        });
+    });
+});
